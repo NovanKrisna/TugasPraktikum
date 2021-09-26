be78571581c1b3e0cef7726cc61c98debbe40a2a
NovanKrisna <novankrisna20@gmail.com>
2021-09-26 20:33:03 +0700

    praktikum

---
+++ Praktikum.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 19:25:08 2021
+
+@author:Novan Krisna
+NIM:065002100030
+"""
+print("menghitung luas ruangan")
+print("-----------------------------")
+p=float(input("masukan panjang ruangan:"))
+l=float(input("masukan lebar ruangan:"))
+s=input("masukan satuan (Meter/inci):")
+persegi_panjang = p*l
+str(print("luas ruangan dengan panjang",p, "dan lebar" ,l," adalah :",persegi_panjang,s))
