99a4d1e8cc6c3af593974fd90b6eb06ee9398954
NovanKrisna <91375357+NovanKrisna@users.noreply.github.com>
2021-09-27 14:06:25 +0700

    Add files via upload

---
+++ praktikum2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 13:19:46 2021
+
+@author: Novan Krisna
+NIM: 065002100030
+"""
+import math
+print("Operasi Aritmatika")
+a=float(input("masukan nilai a:"))
+b=float(input("masukan nilai b:"))
+c=math.log10(a)
+
+
+print("jumlah a ditambah b adalah:",a+b)
+print("selisih antara a dan b adalah:",a-b)
+print("jumlah dari a dikali b adalah:",a*b)
+print("jumlah dari a dibagi b adalah:",a/b)
+print("jumlah sisa pembagian dari hasil a dibagi b adalah:",a%b)
+print("hasil dari log (a)",c)
+print("hasil a pangkat b adalah:",a**b)
